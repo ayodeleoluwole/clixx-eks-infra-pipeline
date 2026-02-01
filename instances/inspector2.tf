85005d3d5698a7e26646d12ce4d15334b49b1623
Oluwole Ayodele <ayodeleoluwole112@gmail.com>
2026-02-01 00:00:00 -0600

    feat: enable AWS Inspector2 for vulnerability scanning

---
+++ instances/inspector2.tf
@@ -0,0 +1,5 @@
+# Enable Inspector2 for this account
+resource "aws_inspector2_enabler" "inspector2" {
+  account_ids = [data.aws_caller_identity.current.account_id]
+  resource_types = ["EC2", "ECR"]
+}
