95e17052a1a00e7321c8af58751a73e88dcdf692
Oluwole Ayodele <ayodeleoluwole112@gmail.com>
2026-02-01 00:00:00 -0600

    chore: add data sources

---
+++ instances/data.tf
@@ -0,0 +1,29 @@
+
+# Declare the data source for availablity zone
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+
+data "aws_caller_identity" "current" {}
+
+
+data "aws_vpc" "jenkins" {
+  filter {
+    name   = "tag:Name"
+    values = ["default"]
+  }
+}
+
+
+# Helm provider is needed to install AWS Load Balancer Controller
+data "aws_eks_cluster_auth" "clixx" {
+  name = aws_eks_cluster.clixx-app.name
+}
+
+
+data "tls_certificate" "clixx" {
+  url = aws_eks_cluster.clixx-app.identity[0].oidc[0].issuer
+}
+
+
